a684719c70e46661b5418c6677f3fc8fe47a2e69
Myles Hathcock <c3ruleant@gmail.com>
2021-07-05 13:08:58 -0500

    finalized content rendering. will port posts next.

--- builder.py
+++ builder.py
@@ -1,17 +1,83 @@
+import sass
+import jinja2
+import mistune
+from pygments import highlight
+from pygments.lexers import get_lexer_by_name
+from pygments.formatters.html import HtmlFormatter
+
 import os
 import shutil
 import logging
 import subprocess
 from pathlib import Path
 from datetime import datetime
+from dataclasses import dataclass
+from typing import Set, Dict, Any, Optional, List
 
-import sass
-import jinja2
 
 DIR = Path(__file__).resolve().parent
 SOURCE = DIR.joinpath("site")
+CONTENT = DIR.joinpath("content")
 BUILD = DIR.joinpath("build")
 log = logging.getLogger("site")
+
+
+class Highlighter(mistune.HTMLRenderer):
+    def block_code(self, code, lang=None):
+        if lang:
+            lexer = get_lexer_by_name(lang)
+            formatter = HtmlFormatter()
+            return highlight(code, lexer, formatter)
+        return "<pre><code>" + mistune.escape(code) + "</code></pre>"
+
+
+RENDERER = mistune.create_markdown(renderer=Highlighter())
+
+
+def generate_url(path: Path):
+    return path.with_suffix(".html").name
+
+
+def consume(obj: Dict[str, Any], name: str, *, convert=lambda x: x):
+    value = obj[name]
+    del obj[name]
+    return convert(value)
+
+
+def datetime_format(value: datetime, fmt: str = "%Y-%m-%d"):
+    return value.strftime(fmt)
+
+
+@dataclass
+class Post:
+    path: Path
+    url: str
+    date: datetime
+    title: str
+    tags: Set[str]
+    extra: Dict[str, Any]
+    content: str
+
+    @staticmethod
+    def load(path: Path) -> "Post":
+        with open(path) as fd:
+            raw = fd.read()
+        fm, content = raw.split("\n---\n", 1)
+        meta = dict()
+        for line in fm.split("\n"):
+            k, v = line.split("=", 1)
+            meta[k.strip()] = v.strip()
+        return Post(
+            path=path,
+            url=generate_url(path),
+            date=consume(
+                meta, "date", convert=lambda v: datetime.strptime(v, "%Y-%m-%d")
+            ),
+            title=consume(meta, "title"),
+            tags=None,
+            extra=meta,
+            content=RENDERER(content),
+        )
 
 
 def git_current_sha() -> str:
@@ -33,19 +99,25 @@ GIT_BRANCH = git_current_branch()
 
 
 class Builder:
-    def __init__(self, source: Path, build: Path, development: bool = True):
+    def __init__(
+        self, source: Path, content: Path, build: Path, development: bool = True
+    ):
         self._source = source
+        self._content = content
         self._build = build
         self._html = jinja2.Environment(
             loader=jinja2.FileSystemLoader(self._source),
             undefined=jinja2.StrictUndefined,
             autoescape=jinja2.select_autoescape(["html", "xml"]),
         )
+        self._html.filters["dt"] = datetime_format
         self._development = development
+        self._posts: List[Post] = list()
 
-    def build_html(self, path: Path):
+    def build_html(self, path: Path, dest: Optional[Path] = None, **kwargs):
         rel = path.relative_to(self._source)
-        dest = self._build.joinpath(rel)
+        if dest is None:
+            dest = self._build.joinpath(rel)
         dest.parent.mkdir(parents=True, exist_ok=True)
         template = self._html.get_template(rel.as_posix())
         now = datetime.utcnow()
@@ -55,11 +127,13 @@ class Builder:
                     year=now.year,
                     date=now.strftime("%Y-%m-%d %H:%M:%S UTC"),
                     ref=GIT_SHA,
-                    reflink=f"https://github.com/ceruleant/ceruleant.github.io/tree/{GIT_SHA}",
+                    reflink=f"https://github.com/my1es/my1es.github.io/tree/{GIT_SHA}",
                     refname=f"{GIT_BRANCH}",
+                    posts=self._posts,
+                    **kwargs,
                 )
             )
-        log.info(f"[html] {rel}")
+        log.info(f"[html] {dest.relative_to(self._build)}")
 
     def build_sass(self, path: Path):
         rel = path.relative_to(self._source)
@@ -88,7 +162,19 @@ class Builder:
             return self.build_sass(path)
         return self.build_copy(path)
 
+    def build_post(self, post: Post):
+        template_path = self._source.joinpath("_post.html")
+        dest = self._build.joinpath(post.url)
+        self.build_html(template_path, dest, post=post)
+
     def build(self):
+        for path in self._content.iterdir():
+            post = Post.load(path)
+            self._posts.append(post)
+        self._posts.sort(key=lambda p: p.date, reverse=True)
+        for post in self._posts:
+            self.build_post(post)
+
         for root, dirs, files in os.walk(self._source):
             for name in files:
                 if name.startswith("_"):
@@ -109,7 +195,7 @@ def main():
         level=logging.DEBUG, format="%(asctime)s %(levelname)s [%(name)s] %(message)s",
     )
 
-    builder = Builder(SOURCE, BUILD)
+    builder = Builder(SOURCE, CONTENT, BUILD)
     builder.build()
 
 
---
+++ content/placeholder.md
@@ -0,0 +1,10 @@
+title = A Placeholder Blog Entry
+date = 2021-07-04
+---
+This is just a placeholder entry. It is _really_ cool.
+
+```rust
+pub fn main() {
+  println!("This is an example.");
+}
+```
